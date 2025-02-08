aa11b5f9b0a9a3b11267efcad2e9fd00b5c83342
Matias <matias.semelman@protonmail.com>
2025-02-08 20:33:43 -0300

    feat: Implementación de autenticación completa
    - Creación de página de login con validaciones
    - Actualización de página de registro con validaciones
    - Agregado de funcionalidad 'recordar credenciales'
    - Implementación de logout en Home
    - Mejoras en la UI/UX con mensajes en español

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, Container } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Register } from './components/auth/Register'
+import { Login } from './components/auth/Login'
 import { Home } from './pages/Home'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </Container>
       </BrowserRouter>
